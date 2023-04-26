88823c42c5f49163973f75a7660dd878bc5bb83e
sara camilo <saraccamilo@gmail.com>
2023-04-26 13:35:15 -0300

    resolucaoq

---
+++ questão01/script.js
@@ -0,0 +1,12 @@
+let INDICE = 13;
+let SOMA = 0;
+let K = 0;
+
+while (K < INDICE) {
+  K++;
+  SOMA += K;
+}
+
+console.log(SOMA);
+
+//no navegador ao apertar F12 e inspecionar, ir em console, colocar este código dará o resultado de 91, esse é o metódo apenas com javascript
